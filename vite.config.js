9ad8cc2890cb2bb41537c563ea9fea51cdd4bc90
Sebastian Fix <sebastian.fix@codebar.ch>
2025-04-06 22:34:09 +0800

    wip

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,9 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: [`resources/views/**/*`],
-        }),
-        tailwindcss(),
-    ],
-    server: {
+    plugins: [laravel({
+        input: ['resources/css/app.css', 'resources/js/app.js'], refresh: [`resources/views/**/*`],
+    }), tailwindcss(),], server: {
         cors: true,
     },
 });
