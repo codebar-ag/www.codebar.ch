661827a15e21e5aad8f70c4b34002f30c84b1bc0
Sebastian Fix <sebastian.fix@codebar.ch>
2025-04-10 12:58:42 +0800

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,11 +26,6 @@
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-    <style>
-        [x-cloak] {
-            display: none !important;
-        }
-    </style>
 </head>
 <body class="font-sans antialiased">
 
