317330ec9a814275b71b9a21e8302f89430cc1f7
Sebastian Fix <sebastian.fix@codebar.ch>
2025-04-10 18:27:51 +0800

    wip

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,7 +25,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
             AddCspHeaders::class,
-            //	AddReferrerPolicyMiddleware::class,
+            AddReferrerPolicyMiddleware::class,
             AddFeaturePolicyHeaders::class,
             SetLanguage::class,
             CacheResponse::class,
