70dc7c2e20970c7986efb87d4cdcaa602859755d
Sebastian Fix <sebastian.fix@codebar.ch>
2025-06-24 00:51:06 +0200

    wip

--- app/Models/Configuration.php
+++ app/Models/Configuration.php
@@ -9,14 +9,11 @@ class Configuration extends Model
 {
     use HasFactory;
 
-    public array $translatable = [];
-
     protected $casts = [
         'section_services' => 'boolean',
         'section_products' => 'boolean',
         'section_technologies' => 'boolean',
         'section_open_source' => 'boolean',
         'links' => 'json',
-        'footer' => 'json',
     ];
 }
