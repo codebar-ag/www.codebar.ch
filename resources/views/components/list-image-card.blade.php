161ae68e123e50a592334b95950ab9fec562f10c
Sebastian Fix <sebastian.fix@codebar.ch>
2025-06-23 22:26:52 +0200

    wip

--- resources/views/components/list-image-card.blade.php
+++ resources/views/components/list-image-card.blade.php
@@ -1,13 +1,12 @@
 @props([
     'image',
-	'imageContainerClassAttributes' => 'h-30 h-30 flex-shrink-0 overflow-hidden',
+	'imageContainerClassAttributes' => 'h-32 h-32 flex-shrink-0 overflow-hidden',
     'name',
     'role' => null,
     'icons' => [],
 ])
 
-<div class="flex rounded-xl overflow-hidden transition
- group">
+<div class="flex flex-row rounded-xl overflow-hidden transition group">
     <div class="{{ $imageContainerClassAttributes }}">
         <img src="{{ $image }}" alt="{{ $name }}"
              class="w-full h-full object-cover transition-transform duration-300 ease-in-out group-hover:scale-105"/>
