8fde13a504ff2c17d6e214a85e2b6faf9e0792ad
Sebastian Fix <sebastian.fix@codebar.ch>
2025-07-30 01:44:22 +0200

    wip

--- config/database.php
+++ config/database.php
@@ -39,6 +39,32 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+        'mysql' => [
+            'driver' => 'mysql',
+            'url' => env('DB_URL'),
+            'read' => [
+                'host' => env('DB_READ_HOST', env('DB_HOST')),
+            ],
+            'write' => [
+                'host' => env('DB_WRITE_HOST', env('DB_HOST')),
+            ],
+            // 'host' => env('DB_HOST'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => env('DB_CHARSET', 'utf8mb4'),
+            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
@@ -59,7 +85,6 @@ return [
             'search_path' => 'public',
             'sslmode' => 'prefer',
         ],
-
     ],
 
     /*
