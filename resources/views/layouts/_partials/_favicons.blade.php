4db33f9fc171b0214e3a3d4d0031303fa7a56490
Sebastian Fix <sebastian.fix@codebar.ch>
2025-07-30 03:13:44 +0200

    wip

--- resources/views/layouts/_partials/_favicons.blade.php
+++ resources/views/layouts/_partials/_favicons.blade.php
@@ -1,11 +1,19 @@
 @props(['manifest' => asset('manifest.json'), 'path' => asset('favicons'), 'color' => '#ffffff'])
 
-<link rel="icon" type="image/png" href="{{ asset('favicons/favicon-96x96.png') }}" sizes="96x96"/>
-<link rel="icon" type="image/svg+xml" href="{{ asset('favicons/favicon.svg') }}"/>
-<link rel="shortcut icon" href="{{ asset('favicons/favicon.ico') }}"/>
-<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicons/apple-touch-icon.png') }}"/>
-<link rel="manifest" href="{{ asset('favicons/site.webmanifest') }}"/>
+@php
+    $prefix = match($configuration?->key) {
+        '_paperflakes' => 'paperflakes',
+        '_codebar' => 'codebar',
+        default => $configuration?->key
+    };
+@endphp
+
+<link rel="icon" type="image/png" href="{{ asset("favicons/{$prefix}/favicon-96x96.png") }}" sizes="96x96"/>
+<link rel="icon" type="image/svg+xml" href="{{ asset("favicons/{$prefix}/favicon.svg") }}"/>
+<link rel="shortcut icon" href="{{ asset("favicons/{$prefix}/favicon.ico") }}"/>
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset("favicons/{$prefix}/apple-touch-icon.png") }}"/>
+<link rel="manifest" href="{{ asset("favicons/{$prefix}/site.webmanifest") }}"/>
 
 <meta name="msapplication-TileColor" content="{{ $color }}"/>
-<meta name="msapplication-TileImage" content="{{ $path.'/ms-icon-144x144.png' }}"/>
+<meta name="msapplication-TileImage" content="{{ asset("favicons/{$prefix}/ms-icon-144x144.png") }}"/>
 <meta name="theme-color" content="{{ $color }}"/>
