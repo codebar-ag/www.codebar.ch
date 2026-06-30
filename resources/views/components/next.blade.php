a1817d43c99392124dc6b92989f70a0107a79714
Sebastian BURGIN-FIX (ext) <sebastian.burginfix_ext+sonepar@sonepar.com>
2026-06-30 14:26:07 +0200

    wip

--- resources/views/components/next.blade.php
+++
@@ -1,9 +0,0 @@
-@if ($configuration?->key === '_codebar')
-    <x-section>
-        <x-h2 :title="__('Next')" />
-        <div class="mt-2 flex flex-col sm:flex-row gap-2 sm:gap-6">
-            <x-a :href="localized_route('about-us.index')" :label="__('Team')" classAttributes="block" />
-            <x-a :href="localized_route('contact.index')" :label="__('Contact')" classAttributes="block" />
-        </div>
-    </x-section>
-@endif
