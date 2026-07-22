3cbfe82303bf647c92f6152dbba2b0c818cf2594
Sebastian BURGIN-FIX (ext) <sebastian.burginfix_ext+sonepar@sonepar.com>
2026-07-22 16:07:49 +0200

    wip

--- routes/console.php
+++ routes/console.php
@@ -3,3 +3,6 @@
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('llm:fetch-analytics')->twiceDaily(5, 17);
+
+Schedule::command('optimize:clear')->twiceDaily(6, 18);
+Schedule::command('responsecache:clear')->twiceDaily(6, 18);
